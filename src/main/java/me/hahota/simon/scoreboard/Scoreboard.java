fdbd822c1c1a493c13340bfd7444b959a7c303f9
KoxSosen <whyconfused2@gmail.com>
2021-03-01 08:03:04 +0100

    Updates by biglev

--- src/main/java/me/hahota/simon/scoreboard/Scoreboard.java
+++ src/main/java/me/hahota/simon/scoreboard/Scoreboard.java
@@ -3,7 +3,6 @@ package me.hahota.simon.scoreboard;
 import net.milkbowl.vault.chat.Chat;
 import net.milkbowl.vault.economy.Economy;
 
-
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
@@ -12,6 +11,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Score;
@@ -19,8 +19,12 @@ import org.bukkit.scoreboard.ScoreboardManager;
 import org.jetbrains.annotations.NotNull;
 
 public final class Scoreboard extends JavaPlugin implements @NotNull Listener {
+
     private static Economy econ = null; // vault economy
     private static Chat chat = null; // vault prefix (? chat)
+    private ScoreboardManager manager; // create scoreboard on startup
+    private org.bukkit.scoreboard.Scoreboard board;
+    private Objective obj;
 
     @Override
     public void onEnable() {
@@ -34,11 +38,10 @@ public final class Scoreboard extends JavaPlugin implements @NotNull Listener {
         setupChat();
         createBoard();
 
-}
+    }
 
     @Override
     public void onDisable() {
-
         // Plugin shutdown logic
     }
 
@@ -50,12 +53,33 @@ public final class Scoreboard extends JavaPlugin implements @NotNull Listener {
         updateBoard(p);
     }
 
-    public void createBoard(){
+    public void createBoard() {
         manager = Bukkit.getScoreboardManager();
         board = manager.getNewScoreboard();
         obj = board.registerNewObjective("asdf", "dummy",
                 ChatColor.translateAlternateColorCodes('&', "   &2Hahota    "));
         obj.setDisplaySlot(DisplaySlot.SIDEBAR);
+    }
+
+    public void setupBoard(Player p) {
+        obj.getScore(format("")).setScore(1);
+        obj.getScore(format("&3Balance: ") + getEcon().getBalance(p)).setScore(2);
+        obj.getScore(format("")).setScore(3);
+        obj.getScore(format("&3Prefix: ") + getChat().getPlayerPrefix(p)).setScore(4);
+        obj.getScore(" ").setScore(5);
+    }
+
+    public void updateBoard(Player p) {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                obj.getScore(format("")).setScore(1);
+                obj.getScore(format("&3Balance: ") + getEcon().getBalance(p)).setScore(2);
+                obj.getScore(format("")).setScore(3);
+                obj.getScore(format("&3Prefix: ") + getChat().getPlayerPrefix(p)).setScore(4);
+                obj.getScore(" ").setScore(5);
+            }
+        }.runTaskTimer(this, 600, 1);
     }
 
     private boolean setupEconomy() {
@@ -82,5 +106,9 @@ public final class Scoreboard extends JavaPlugin implements @NotNull Listener {
 
     public static Chat getChat() {
         return chat;
+    }
+
+    private String format(String message) {
+        return ChatColor.translateAlternateColorCodes('&', message);
     }
 }
